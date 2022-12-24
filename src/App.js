9c876455316b5b99b0528a2078475b1da19f4577
PedroHombre <radekgornackiv2@gmail.com>
2022-12-24 12:45:47 +0100

    basic footer component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import './scss/global.scss';
 
 function App() {
 	return (
 		<>
 			<Header />
+
+			<div className="mainAppContainer"></div>
+
+			<Footer />
 		</>
 	);
 }
