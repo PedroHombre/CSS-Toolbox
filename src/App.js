2c95e965336f416e03519bfadd2f20f325990f54
PedroHombre <radekgornackiv2@gmail.com>
2023-01-01 20:47:55 +0100

    Basic pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+
+// Pages import
+import Home from './pages/Home/Home';
+
+// Generator pages import
+import BoxShadowGen from './pages/generators/boxShadow/BoxShadow';
+
 import './scss/global.scss';
 
 function App() {
@@ -7,7 +15,16 @@ function App() {
 		<>
 			<Header />
 
-			<div className="mainAppContainer"></div>
+			<main>
+				<BrowserRouter>
+					<Routes>
+						<Route path="/">
+							<Route index element={<Home />} />
+							<Route path="box-shadow" element={<BoxShadowGen />} />
+						</Route>
+					</Routes>
+				</BrowserRouter>
+			</main>
 
 			<Footer />
 		</>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Logo from "../Logo/Logo";
 
 const Header = () => {
     return (
         <header>
-            <Logo />
+            <Link to="/">
+                <Logo />
+            </Link>
         </header>
     );
 }
