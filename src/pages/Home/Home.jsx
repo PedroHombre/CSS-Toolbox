94854b583ca27b7e90f805e0f3d1e9672094dbe7
PedroHombre <radekgornackiv2@gmail.com>
2023-01-02 23:40:33 +0100

    modifications to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import './Home.scss';
 
 const Home = () => {
     return (
-        <>
-            Home
-        </>
+        <main className="home-page">
+            <h2>
+                CSS Toolbox
+            </h2>
+
+            {/* TODO: Change this paragraph */}
+            <p>
+                A simple generator wich you can use to generate CSS properties. Allows simple and convenient adjustments of parameters using sliders. Features live preview and an easy way to copy generated CSS code.
+            </p>
+
+            <div className="buttons-container">
+                <Link to="/box-shadow" >
+                    Box shadow
+                </Link>
+            </div>
+        </main>
     )
 }
 
